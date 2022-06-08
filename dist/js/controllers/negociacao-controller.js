aa15137661926d6cdfac03df97a97c8828b7d146
Victor <victorsantos.hf@gmail.com>
2022-06-08 11:11:38 -0300

    pasta dist

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,24 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limpaForm();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    limpaForm() {
+        document.querySelector('form').reset();
+        this.inputData.focus();
+    }
+}
