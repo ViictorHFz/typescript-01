9892fc63d21dc9919525dc4d7b850cd7ee2d21e8
Victor <victorsantos.hf@gmail.com>
2022-06-22 00:04:29 -0300

    Property Decorator

---
+++ app/src/decorators/dom-injection.ts
@@ -0,0 +1,17 @@
+export function domInjector(seletor: string) {
+  return function (target: any, propertyKey: string) {
+    let elemento: HTMLElement;
+    const getter = function () {
+      if (!elemento) {
+        elemento = document.querySelector(seletor) as HTMLElement;
+        console.log(`buscando elemento do DOM com o seletor ${seletor} para injetar no ${propertyKey}`);
+      }
+
+      return elemento;
+    };
+
+    Object.defineProperty(target, propertyKey, {
+      get: getter,
+    });
+  };
+}
